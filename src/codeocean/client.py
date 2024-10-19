ce29c8e665a1bc388187316bb8b93397ac88a9e9
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2024-10-19 14:00:01 -0700

    feat: adds option for custom adapters

--- src/codeocean/client.py
+++ src/codeocean/client.py
@@ -1,6 +1,9 @@
+from typing import Tuple
+
 from requests_toolbelt.sessions import BaseUrlSession
 from requests_toolbelt.adapters.socket_options import TCPKeepAliveAdapter
-from dataclasses import dataclass
+from requests.adapters import HTTPAdapter
+from dataclasses import dataclass, field
 
 from codeocean.capsule import Capsules
 from codeocean.computation import Computations
@@ -12,11 +15,15 @@ class CodeOcean:
 
     domain: str
     token: str
+    _optional_adapters: Tuple[HTTPAdapter] = field(default=tuple(), repr=False)
 
     def __post_init__(self):
         self.session = BaseUrlSession(base_url=f"{self.domain}/api/v1/")
         self.session.auth = (self.token, "")
         self.session.mount(self.domain, TCPKeepAliveAdapter())
+
+        for optional_adapter in self._optional_adapters:
+            self.session.mount(self.domain, optional_adapter)
         self.session.headers.update({"Content-Type": "application/json"})
         self.session.hooks["response"] = [
             lambda response, *args, **kwargs: response.raise_for_status()
