86a256a84c96f600c6c81536a8a9b0d9d3a60ea6
Zvika Gart <zvikagart@gmail.com>
2024-10-21 12:02:11 +0100

    Fixes

--- src/codeocean/client.py
+++ src/codeocean/client.py
@@ -1,9 +1,9 @@
-from typing import Tuple
-
-from requests_toolbelt.sessions import BaseUrlSession
+from dataclasses import dataclass
 from requests_toolbelt.adapters.socket_options import TCPKeepAliveAdapter
+from requests_toolbelt.sessions import BaseUrlSession
 from requests.adapters import HTTPAdapter
-from dataclasses import dataclass, field
+from typing import Optional
+from urllib3.util import Retry
 
 from codeocean.capsule import Capsules
 from codeocean.computation import Computations
@@ -15,20 +15,17 @@ class CodeOcean:
 
     domain: str
     token: str
-    _optional_adapters: Tuple[HTTPAdapter] = field(default=tuple(), repr=False)
+    retries: Optional[Retry] = None
 
     def __post_init__(self):
         self.session = BaseUrlSession(base_url=f"{self.domain}/api/v1/")
         self.session.auth = (self.token, "")
-        self.session.mount(self.domain, TCPKeepAliveAdapter())
-
-        for optional_adapter in self._optional_adapters:
-            self.session.mount(self.domain, optional_adapter)
         self.session.headers.update({"Content-Type": "application/json"})
         self.session.hooks["response"] = [
             lambda response, *args, **kwargs: response.raise_for_status()
         ]
-
+        self.session.mount(self.domain, TCPKeepAliveAdapter(max_retries=self.retries))
+        
         self.capsules = Capsules(client=self.session)
         self.computations = Computations(client=self.session)
         self.data_assets = DataAssets(client=self.session)
