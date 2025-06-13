5628895ac9d8c5e84a0a4d5cd7541c17ca49bea1
jake-valsamis <jake@codeocean.com>
2025-06-13 17:43:44 -0400

    add error handling to client.py using structure defined in errors.py

--- src/codeocean/client.py
+++ src/codeocean/client.py
@@ -3,12 +3,21 @@ from __future__ import annotations
 from dataclasses import dataclass
 from requests_toolbelt.adapters.socket_options import TCPKeepAliveAdapter
 from requests_toolbelt.sessions import BaseUrlSession
-from typing import Optional
+from typing import Optional, Union
 from urllib3.util import Retry
+import requests
 
 from codeocean.capsule import Capsules
 from codeocean.computation import Computations
 from codeocean.data_asset import DataAssets
+from codeocean.errors import (
+    BadRequestError,
+    UnauthorizedError,
+    ForbiddenError,
+    NotFoundError,
+    InternalServerError,
+    CodeOceanError
+)
 
 
 @dataclass
@@ -45,11 +54,34 @@ class CodeOcean:
         })
         if self.agent_id:
             self.session.headers.update({"Agent-Id": self.agent_id})
-        self.session.hooks["response"] = [
-            lambda response, *args, **kwargs: response.raise_for_status()
-        ]
+        self.session.hooks["response"] = [self.error_handler]
         self.session.mount(self.domain, TCPKeepAliveAdapter(max_retries=self.retries))
 
         self.capsules = Capsules(client=self.session)
         self.computations = Computations(client=self.session)
         self.data_assets = DataAssets(client=self.session)
+
+    def error_handler(self, response, *args, **kwargs):
+        try:
+            response.raise_for_status()
+        except requests.HTTPError as ex:
+            try:
+                error_payload = response.json()
+                if not isinstance(error_payload, dict):
+                    raise ValueError("Response is not a JSON object")
+            except ValueError:
+                error_payload = {"message": response.text or str(ex)}
+
+            status_code = response.status_code
+            if status_code == 400:
+                raise BadRequestError.from_dict(error_payload).with_error(ex)
+            elif status_code == 401:
+                raise UnauthorizedError.from_dict(error_payload).with_error(ex)
+            elif status_code == 403:
+                raise ForbiddenError.from_dict(error_payload).with_error(ex)
+            elif status_code == 404:
+                raise NotFoundError.from_dict(error_payload).with_error(ex)
+            elif status_code >= 500:
+                raise InternalServerError.from_dict(error_payload).with_error(ex)
+            else:
+                raise CodeOceanError.from_dict(error_payload).with_error(ex)
